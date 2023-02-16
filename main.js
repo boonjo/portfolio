750ecceeab305205a8660dfe5ac3fb3ee60e45cb
boojo <jshim27@wisc.edu>
2023-02-15 23:06:46 -0600

    sry

--- main.js
+++ main.js
@@ -1,37 +1,6 @@
-// Add an event listener to the navigation links to highlight the selected link
-const navLinks = document.querySelectorAll('.siteHeader_nav a');
-navLinks.forEach((link) => {
-  link.addEventListener('click', () => {
-    navLinks.forEach((otherLink) => {
-      otherLink.classList.remove('is-selected');
-    });
-    link.classList.add('is-selected');
-  });
-});
-
 const enterView = document.querySelector('#EnterView');
 enterView.style.opacity = '0';
 enterView.style.transition = 'opacity 1s';
 setTimeout(() => {
   enterView.style.display = 'none';
 }, 2000);
-
-
-// Create a Vue app to toggle the theme
-// const app = Vue.createApp({
-//   data() {
-//     return {
-//       ui: {
-//         theme: 'light',
-//       },
-//     };
-//   },
-//   methods: {
-//     toggleTheme() {
-//       this.ui.theme = this.ui.theme === 'light' ? 'dark' : 'light';
-//     },
-//   },
-// });
-
-// Mount the Vue app to the #Theme element
-// app.mount('#Theme');
