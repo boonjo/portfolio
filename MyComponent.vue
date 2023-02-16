4514bad0d2ce164bebfb0e30131067f624c49a8e
boojo <jshim27@wisc.edu>
2023-02-15 23:31:02 -0600

    okay idk what's going on now

---
+++ MyComponent.vue
@@ -0,0 +1,22 @@
+<!-- MyComponent.vue -->
+<template>
+  <div>
+    <button @click="toggleTheme">Toggle theme</button>
+    <div :class="{ 'is-selected': $store.state.ui.theme === 'light' }">Light theme</div>
+    <div :class="{ 'is-selected': $store.state.ui.theme === 'dark' }">Dark theme</div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    toggleTheme() {
+      if (this.$store.state.ui.theme === 'light') {
+        this.$store.commit('setTheme', 'dark');
+      } else {
+        this.$store.commit('setTheme', 'light');
+      }
+    },
+  },
+};
+</script>
