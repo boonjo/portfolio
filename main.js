688bdf16574a9035196524d8a4732a5345ff9766
boojo <jshim27@wisc.edu>
2023-02-15 23:32:33 -0600

    back to og

--- main.js
+++ main.js
@@ -1,13 +1,3 @@
-import Vue from 'vue';
-import store from './store';
-import MyComponent from './MyComponent.vue';
-
-new Vue({
-  store,
-  components: { MyComponent },
-  template: '<my-component></my-component>',
-}).$mount('#app');
-
 const enterView = document.querySelector('#EnterView');
 enterView.style.opacity = '0';
 enterView.style.transition = 'opacity 1s';
