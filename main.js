75fea31a8ce5c7b7a52db0769d576784080bf993
boojo <jshim27@wisc.edu>
2023-02-25 14:46:02 -0600

    4 seconds

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // hide the EnterView element after 3 seconds
 const enterView = document.querySelector('#EnterView');
 enterView.style.opacity = '0';
-enterView.style.transition = 'opacity 3s';
+enterView.style.transition = 'opacity 4s';
 setTimeout(() => {
   enterView.style.display = 'none';
-}, 3000);
+}, 4000);
