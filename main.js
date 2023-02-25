baf6eb7e488f9d4d27214f4828656812f29680e6
boojo <jshim27@wisc.edu>
2023-02-25 14:53:00 -0600

    plz?

--- main.js
+++ main.js
@@ -17,11 +17,3 @@ const store = new Vuex.Store({
 const app = createApp({});
 app.use(store);
 app.mount('#app');
-
-// hide the EnterView element after 3 seconds
-const enterView = document.querySelector('#EnterView');
-enterView.style.opacity = '0';
-enterView.style.transition = 'opacity 3.5s';
-setTimeout(() => {
-  enterView.style.display = 'none';
-}, 3500);
