4a36a3bbf884214052e477bfdf1ec7375f056748
boojo <jshim27@wisc.edu>
2023-02-25 14:47:57 -0600

    this crap again

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
+import { createApp } from 'vue';
+import Vuex from 'vuex';
+
+const store = new Vuex.Store({
+  state: {
+    ui: {
+      theme: 'light'
+    }
+  },
+  mutations: {
+    toggleTheme(state) {
+      state.ui.theme = state.ui.theme === 'light' ? 'dark' : 'light';
+    }
+  }
+});
+
+const app = createApp({});
+app.use(store);
+app.mount('#app');
+
 // hide the EnterView element after 3 seconds
 const enterView = document.querySelector('#EnterView');
 enterView.style.opacity = '0';
