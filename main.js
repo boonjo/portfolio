5fdf00e9d41927103959f282fcd4e6c202fffba0
boojo <jshim27@wisc.edu>
2023-02-25 14:35:22 -0600

    Uh not so sure about this

--- main.js
+++ main.js
@@ -2,11 +2,18 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import store from './store';
 
-createApp(App).use(store).mount('#app');
+const app = createApp(App);
 
+// Use the Vuex store in the app
+app.use(store);
+
+// Hide the EnterView after 3 seconds
 const enterView = document.querySelector('#EnterView');
 enterView.style.opacity = '0';
 enterView.style.transition = 'opacity 3s';
 setTimeout(() => {
   enterView.style.display = 'none';
 }, 3000);
+
+// Mount the app to the DOM
+app.mount('#app');
