4f23f994f8ce2c995b4246358e40407aa866a1e1
boojo <jshim27@wisc.edu>
2023-02-25 14:37:02 -0600

    this time with no imports

--- main.js
+++ main.js
@@ -1,19 +1,23 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import store from './store';
+// define the app instance
+const app = Vue.createApp({
+  // app options here
+});
 
-const app = createApp(App);
+// define the store instance
+const store = Vuex.createStore({
+  // store options here
+});
 
-// Use the Vuex store in the app
+// mount the store to the app
 app.use(store);
 
-// Hide the EnterView after 3 seconds
+// mount the app to the DOM
+app.mount('#app');
+
+// hide the EnterView element after 3 seconds
 const enterView = document.querySelector('#EnterView');
 enterView.style.opacity = '0';
 enterView.style.transition = 'opacity 3s';
 setTimeout(() => {
   enterView.style.display = 'none';
 }, 3000);
-
-// Mount the app to the DOM
-app.mount('#app');
