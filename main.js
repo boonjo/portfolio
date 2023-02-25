8c7f1e61873fd2480c864c0fa1b4866f74db63fc
boojo <jshim27@wisc.edu>
2023-02-25 14:29:33 -0600

    will this work

--- main.js
+++ main.js
@@ -13,11 +13,3 @@ export default new Vuex.Store({
     }
   }
 })
-
-
-const enterView = document.querySelector('#EnterView');
-enterView.style.opacity = '0';
-enterView.style.transition = 'opacity 3s';
-setTimeout(() => {
-  enterView.style.display = 'none';
-}, 3000);
