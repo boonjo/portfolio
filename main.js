27cdb1c92521696de1e994fa5c559bb48bcdcd54
boojo <jshim27@wisc.edu>
2023-02-25 16:35:24 -0600

    stuff changes that I'm desparate for

--- main.js
+++ main.js
@@ -7,3 +7,22 @@ new Vue({
   store, // add the store to the Vue instance
   render: h => h(App)
 });
+
+const store = Vuex.createStore({
+  state() {
+      return {
+          ui: {
+              theme: 'light',
+          },       
+      };
+  },
+  mutations: {
+      toggleTheme(state) {
+          state.ui.theme = state.ui.theme === 'light' ? 'dark' : 'light';
+      },
+  },
+});
+
+const app = Vue.createApp({});
+app.use(store);
+app.mount('#app');
