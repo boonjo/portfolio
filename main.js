5e3d4a16dcc88787295b75089d19d44558f49d34
boojo <jshim27@wisc.edu>
2023-02-25 14:22:26 -0600

    vue and vuex in js

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    theme: 'light'
+  },
+  mutations: {
+    toggleTheme(state) {
+      state.theme = state.theme === 'light' ? 'dark' : 'light'
+    }
+  }
+})
+
+
 const enterView = document.querySelector('#EnterView');
 enterView.style.opacity = '0';
 enterView.style.transition = 'opacity 3s';
