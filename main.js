641df711a24b4ea8b0e8a81d5b49513e027c0c89
boojo <jshim27@wisc.edu>
2023-03-23 14:24:46 -0500

    one page html now

--- main.js
+++ main.js
@@ -6,3 +6,11 @@ function hideEnterView() {
       enterView.style.display = 'none';
     }, 3500);
   }
+
+  function show(shown, hidden1, hidden2, hidden3) {
+    document.getElementById(shown).style.display='block';
+    document.getElementById(hidden1).style.display='none';
+    document.getElementById(hidden2).style.display='none';
+    document.getElementById(hidden3).style.display='none';
+    return false;
+  }
