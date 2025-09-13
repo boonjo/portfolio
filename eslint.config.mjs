006f5fb815135582b676eac70904e6175d5d334b
boojo <jshim27@wisc.edu>
2025-09-12 23:56:28 -0500

    testing new eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,41 +1,22 @@
-import { fileURLToPath } from "url";
-import { dirname } from "path";
+// eslint.config.mjs
+import eslintPluginPrettier from "eslint-plugin-prettier";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-/** @type {import("eslint").ESLint.ConfigData} */
-const config = {
-  root: true,
-  extends: [
-    "next/core-web-vitals",
-    "next/typescript",
-    "plugin:prettier/recommended"
-  ],
-  parserOptions: {
-    tsconfigRootDir: __dirname,
-    project: "./tsconfig.json",
-  },
-  rules: {
-    /// TypeScript
-    "@typescript-eslint/explicit-function-return-type": "off", // don’t require return types everywhere
-    "@typescript-eslint/no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }], // warn about unused vars, ignore ones starting with _
-
-    // General
-    "no-console": ["warn", { allow: ["warn", "error"] }], // warn instead of error for console.log
-    "no-debugger": "error", // forbid debugger statements
-
-    // Prettier
-    "prettier/prettier": [
-      "error",
-      {
-        singleQuote: true,
-        semi: true,
-        trailingComma: "all",
-        printWidth: 100,
+export default [
+  {
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
       },
-    ],
+    },
+    plugins: {
+      prettier: eslintPluginPrettier,
+    },
+    extends: ["next/core-web-vitals", "next/typescript", "plugin:prettier/recommended"],
+    rules: {
+      "no-console": "warn",
+      "prettier/prettier": ["error", { singleQuote: true, semi: true }],
+    },
   },
-};
-
-export default config;
+];
