61c91f182ce7124bd917e1540357da466556e5f3
boojo <jshim27@wisc.edu>
2025-09-12 23:05:32 -0500

    the pursuit of deployment

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,11 @@ import createMDX from '@next/mdx';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
 };
+module.exports = nextConfig
 
 const withMDX = createMDX({
   extension: /\.mdx?$/,
